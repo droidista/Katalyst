48d0a3da19b1e532fea83226810748a0a40b779c
Anand Bose <anandbose16@gmail.com>
2023-04-24 21:38:49 +0530

    Fix dependency resolution logic of InlineStyleGenerator

--- katalyst/src/main/kotlin/com/droidista/katalyst/generator/InlineStyleGenerator.kt
+++ katalyst/src/main/kotlin/com/droidista/katalyst/generator/InlineStyleGenerator.kt
@@ -12,8 +12,8 @@ import java.util.Stack
 class InlineStyleGenerator(val cssDefinitionList: List<CssDefinition>) : DeferredGenerator {
     override fun generate(root: Node, environment: Environment): Element {
         val matchingDefinitions = extractMatchingStylesheets(root)
-        resolveDependencies(matchingDefinitions)
-        val generatedCss = matchingDefinitions.joinToString(separator = " ") { it.render() }
+        val definitionsWithDependencies = resolveDependencies(matchingDefinitions)
+        val generatedCss = definitionsWithDependencies.joinToString(separator = " ") { it.render() }
         val node = Node("style")
         node.children = mutableListOf(
             Text(text = generatedCss, parent = node)
@@ -60,31 +60,23 @@ class InlineStyleGenerator(val cssDefinitionList: List<CssDefinition>) : Deferre
         return cssSet
     }
 
-    private fun resolveDependencies(definitions: MutableSet<CssDefinition>) {
-        var iterator = definitions.iterator()
-        var dependencies = mutableSetOf<CssDefinition>()
-        while (true) {
-            if (iterator.hasNext()) {
-                val definition = iterator.next()
-                val dependencyTags = definition.depends
-                if (!dependencyTags.isNullOrEmpty()) {
-                    dependencyTags.forEach { requiredDependencyTag ->
-                        val list = cssDefinitionList.filter { externalDefinition ->
-                            externalDefinition.dependencyTag == requiredDependencyTag
-                        }
-                        dependencies.addAll(list)
+    private fun resolveDependencies(definitions: Collection<CssDefinition>): Set<CssDefinition> {
+        val list = mutableListOf<CssDefinition>()
+        list.addAll(definitions)
+        var index = 0
+        while (index < list.size) {
+            val definition = list[index]
+            val dependencyTags = definition.depends
+            if (!dependencyTags.isNullOrEmpty()) {
+                dependencyTags.forEach { requiredDependencyTag ->
+                    val dependencies = cssDefinitionList.filter { externalDefinition ->
+                        externalDefinition.dependencyTag == requiredDependencyTag
                     }
-                }
-            } else {
-                if (dependencies.isNotEmpty()) {
-                    definitions.addAll(dependencies)
-                    iterator = dependencies.iterator()
-                    dependencies = mutableSetOf()
-                    continue
-                } else {
-                    break
+                    list.addAll(dependencies - list.toSet())
                 }
             }
+            index++
         }
+        return list.toSet()
     }
 }
